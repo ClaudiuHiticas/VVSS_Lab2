1ea362a5d77076428eaabfe591edda82b750d9fb
Hiticas Claudiu <hiticas.claudiu@gmail.com>
2021-04-06 16:24:57 +0300

    test integration

--- src/test/java/Lab2Ok/AppTestIntegartion.java
+++ src/test/java/Lab2Ok/AppTestIntegartion.java
@@ -1,6 +1,8 @@
 package Lab2Ok;
 
+import Lab2Ok.domain.Nota;
 import Lab2Ok.domain.Student;
+import Lab2Ok.domain.Tema;
 import Lab2Ok.repository.NotaXMLRepo;
 import Lab2Ok.repository.StudentXMLRepo;
 import Lab2Ok.repository.TemaXMLRepo;
@@ -12,6 +14,7 @@ import Lab2Ok.validation.ValidationException;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -20,10 +23,28 @@ import static org.junit.Assert.assertThat;
 public class AppTestIntegartion extends TestCase {
     private String filenameStudent = "fisiere/test/Studenti.xml";
     private String filenameTema = "fisiere/test/Teme.xml";
+    private String filenameNote = "fisiere/test/Note.xml";
+
+    private TemaValidator temaValidator = new TemaValidator();
+    private TemaXMLRepo temaXMLRepo = new TemaXMLRepo(filenameTema);
+
     private StudentValidator studentValidator = new StudentValidator();
     private StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
-    Service service = new Service(studentXMLRepository, studentValidator, null, null, null, null);
 
+    private NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepo);
+    private NotaXMLRepo notaXMLRepo = new NotaXMLRepo(filenameNote);
+
+    Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepo, temaValidator, notaXMLRepo, notaValidator);
+
+
+    private void cleanUpTema() {
+        ArrayList<Tema> list = new ArrayList<Tema>();
+        service.getAllTeme().forEach(list::add);
+        for (Tema tema :
+                list) {
+            service.deleteTema(tema.getID());
+        }
+    }
 
     private void cleanUpStudent() {
         ArrayList<Student> list = new ArrayList<Student>();
@@ -47,10 +68,34 @@ public class AppTestIntegartion extends TestCase {
 
 
     @Test
-    public void test_add_student_2() {
+    public void test_add_tema() {
+        cleanUpTema();
+        Tema tema = new Tema("1", "", 10, 12);
+        try {
+            service.addTema(tema);
+        } catch (ValidationException validationException) {
+            assertThat(validationException.getMessage(), is("Descriere invalida!"));
+        }
+    }
+
+    @Test
+    public void test_add_grade() {
+        service.deleteNota("1111");
+        Tema tema = new Tema("12111", "Lab2", 5, 2);
+        service.addTema(tema);
+        Student student = new Student("121", "nu", 11, "nu@n.com");
+        service.addStudent(student);
+        Nota nota = new Nota("1111", "121", "12111", 9, LocalDate.of(2018,11,1));
+        assertNull(service.addNota(nota, "super gucci"));
+        cleanUpTema();
         cleanUpStudent();
-        Student student = new Student("111", "nume", 933, "nume@nume.com");
-        assertNull(service.addStudent(student));
+    }
+
+    @Test
+    public void test_integration(){
+        test_add_tema();
+        test_add_student();
+        test_add_grade();
     }
 
 
